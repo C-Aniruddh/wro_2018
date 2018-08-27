da3e218d64b5f6c3d9b1c4127a0ffb62c0ef00eb
C-Aniruddh <aniruddh.chandratre@gmail.com>
2018-08-27 15:46:29 +0530

    wro_2018: push camera pickup file

---
+++ calculations.py
@@ -0,0 +1,31 @@
+import config
+import numpy as np
+from numpy.linalg import inv
+
+cameraMatrix = config.cameraMatrix
+distortionCoeffs = config.distortionCoeffs
+rvct = config.rvct
+tvct = config.tvct
+distance_cam = config.distance_cam
+
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+def world_coordinates(u, v):
+    world_coord = np.array([[u], [v], [1]])
+    world_coord = inv(cameraMatrix) * world_coord
+    world_coord = world_coord * distance_cam
+    world_x = world_coord[0][0]
+    world_y = world_coord[1][1]
+    print(world_x, world_y)
+    return world_x, world_y
---
+++ config.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# Video
+FPS = 30
+CAMERA_ID = 1
+
+# Serial
+ARDUINO_SERIAL_PORT = "/dev/tty"
+BAUD_RATE = 9600
+
+# 3x3
+cameraMatrix = np.array([[807.436816003674, 0., 0.],
+                         [2.03900754532943, 807.610112298853, 0.],
+                         [302.651456007652, 263.724593595848, 1.]])
+
+# 5 values
+distortionCoeffs = np.array([1.87840399e-01, -1.34123291e+00, 1.21747252e-03, -2.00202920e-03, 2.23674740e+00])
+
+# 3 values
+rvct = np.array([-0.10056308, 0.00848036, 1.59080042])
+
+# 3 values
+tvct = np.array([37.47226676, -68.12233158, 256.68262234])
+
+# whatever unit is chosen here, the coordinates will be in the same unit
+distance_cam = 270
+
+# Configure min and max servo pulse lengths
+servo_min = 150  # Min pulse length out of 4096
+servo_max = 600  # Max pulse length out of 4096
